662b822a12492330bb0862347dcf6ecd260d49bb
Prasoon Ratnakar <prasoonratnakar06@gmail.com>
2026-06-11 23:31:18 +0530

    Add files via upload

---
+++ Lecture03/simpleintrest.c
@@ -0,0 +1,10 @@
+	#include <stdio.h>
+	void main ()
+	{
+		float p,r,t,si;
+		printf("Enter the value of p,r,t");
+		scanf("%f%f%f",&p,&r,&t);
+		si=(p*r*t)/100;
+		printf("Simple intrest = %f",si);
+		
+	}
